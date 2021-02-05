8edf256e8a549d155ff8fbc5b6a41beba88a47f9
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-05 19:35:23 +0900

    yarn ng g c features/top/containers/top

--- src/app/features/top/top.module.ts
+++ src/app/features/top/top.module.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TopComponent } from './containers/top/top.component';
 import { TopPageComponent } from './pages/top/top.component';
 
 @NgModule({
-  declarations: [TopPageComponent],
+  declarations: [TopPageComponent, TopComponent],
   imports: [CommonModule],
 })
 export class TopModule {}
