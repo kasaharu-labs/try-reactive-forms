3b20fcb2aee18dfc0fdb9466d2a0c71e08f55d1d
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-05 19:20:24 +0900

    yarn ng g c features/top/pages/top

--- src/app/features/top/top.module.ts
+++ src/app/features/top/top.module.ts
@@ -1,8 +1,9 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { TopComponent } from './pages/top/top.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [TopComponent],
   imports: [
     CommonModule,
   ],
