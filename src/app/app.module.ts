eecddda0e971e988623fe42a5e542715e1ffd80a
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-05 19:41:34 +0900

    feat: Top container component 表示

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopModule } from './features/top/top.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, TopModule],
   providers: [],
   bootstrap: [AppComponent],
 })
