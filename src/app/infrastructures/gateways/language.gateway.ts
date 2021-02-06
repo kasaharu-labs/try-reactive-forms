cc1af83bb94d045eb5ed233886c29204f506c77a
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-07 08:06:47 +0900

    feat: 言語の case を返す gateway 関数を実装

--- src/app/infrastructures/gateways/language.gateway.ts
+++ src/app/infrastructures/gateways/language.gateway.ts
@@ -1,15 +1,26 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+export interface LanguageCase {
+  id: number;
+  case: string;
+  languageId: number;
+}
+
 @Injectable({
   providedIn: 'root',
 })
 export class LanguageGateway {
   constructor() {}
   private _langulages$ = new BehaviorSubject<any>(null);
+  private _languageCases$ = new BehaviorSubject<any>(null);
 
   get langulages$() {
     return this._langulages$.asObservable();
+  }
+
+  get langulageCases$() {
+    return this._languageCases$.asObservable();
   }
 
   fetchLanguages(): void {
@@ -19,5 +30,17 @@ export class LanguageGateway {
     ];
 
     this._langulages$.next(languages);
+  }
+
+  fetchLanguageCases(languageId: number): void {
+    const languageCases: LanguageCase[] = [
+      { id: 1, case: 'ひらがな', languageId: 1 },
+      { id: 2, case: 'カタカナ', languageId: 1 },
+      { id: 3, case: '大文字', languageId: 2 },
+      { id: 4, case: '小文字', languageId: 2 },
+    ];
+
+    const targetCases = languageCases.filter((languageCase) => languageCase.languageId === languageId);
+    this._languageCases$.next(targetCases);
   }
 }
