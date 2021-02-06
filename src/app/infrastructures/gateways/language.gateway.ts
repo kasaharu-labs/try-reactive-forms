6cdb2a481965ede31d896acafc0b662773fc605c
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-06 12:58:17 +0900

    feat: 言語情報を返す gateway 関数を実装

--- src/app/infrastructures/gateways/language.gateway.ts
+++ src/app/infrastructures/gateways/language.gateway.ts
@@ -1,8 +1,23 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class LanguageGateway {
   constructor() {}
+  private _langulages$ = new BehaviorSubject<any>(null);
+
+  get langulages$() {
+    return this._langulages$.asObservable();
+  }
+
+  fetchLanguages(): void {
+    const languages = [
+      { id: 1, name: '日本語' },
+      { id: 2, name: 'English' },
+    ];
+
+    this._langulages$.next(languages);
+  }
 }
