f0f26acf9d9ae4e00c9afee29e8b7ea985e62984
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-07 12:14:01 +0900

    feat: User 情報を取得する gateway 関数を実装

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
@@ -6,7 +7,7 @@ import { TopModule } from './features/top/top.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, TopModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule, TopModule],
   providers: [],
   bootstrap: [AppComponent],
 })
