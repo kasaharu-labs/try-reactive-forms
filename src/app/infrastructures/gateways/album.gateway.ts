385305b66672bb276603d4a2cd1965c1bab1bd06
Wataru.Kasahara <kasahara1019@gmail.com>
2021-02-07 13:40:18 +0900

    feat: Album 情報を取得する gateway 関数を実装

--- src/app/infrastructures/gateways/album.gateway.ts
+++ src/app/infrastructures/gateways/album.gateway.ts
@@ -1,8 +1,15 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Album } from '../../domain/album';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AlbumGateway {
-  constructor() {}
+  constructor(private readonly _http: HttpClient) {}
+
+  getAlbums(userId: number): Observable<Album[]> {
+    return this._http.get<Album[]>(`https://jsonplaceholder.typicode.com/albums?userId=${userId}`);
+  }
 }
